a4450636b3ec5f88a791d734c412c22b5fcae868
Erfan <erfansefat@outlook.com>
2024-06-21 23:37:18 +0330

    data fetching and card optimization

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -23,12 +23,12 @@ const ProjectCard = ({
 }) => {
   return (
     <div
-      onClick={()=> window.open(link)}
+      onClick={() => window.open(link)}
       onMouseOver={overfunc}
       onMouseLeave={leavefunc}
       className="w-fit projectCard p-[1px] bg-gradient-to-br from-white-1 via-white-2 to-white-3 rounded-[16px] backdrop-blur-xl cursor-pointer hover:drop-shadow-rich4 hover:scale-105"
     >
-      <div className="px-[8px] pt-[8px] pb-[13px] inline-flex flex-col items-center bg-card-background  rounded-[15px]	relative">
+      <div className="h-[100%] px-[8px] pt-[8px] pb-[13px] inline-flex flex-col items-center bg-card-background  rounded-[15px]	relative">
         <Image
           className="rounded-[9px] mb-3 drop-shadow-rich1"
           src={imgsrc}
@@ -36,7 +36,7 @@ const ProjectCard = ({
           height={252}
           alt={name}
         />
-        <div className="m-[1px] bg-black rounded-[6px] absolute h-6 w-14 blur-lg right-4 top-7 opacity-80"></div>
+        <div className="m-[1px] bg-black rounded-[6px] absolute h-6 w-14 blur-lg right-2 top-7 opacity-30"></div>
         <div className="bg-gradient-to-br from-white-1 via-white-4 to-white-3 rounded-[7px] backdrop-blur-[3px] drop-shadow-rich3 absolute right-4 top-4">
           <div className="m-[1px] bg-card-background2 rounded-[6px] px-2 py-1 text-[11px]">
             {type}
@@ -44,11 +44,11 @@ const ProjectCard = ({
         </div>
 
         <div className="w-[320px]">
-          <div className="text-base">{name}</div>
-          <div className="flex text-sm justify-between opacity-50">
-            <div>{desc}</div>
-            <div>{year}</div>
+          <div className="flex items-end justify-between mb-1.5 font-light">
+            <div>{name}</div>
+            <div className="text-[11px] border px-2 py-[1px] rounded-md opacity-50 -mr-1.5">{year}</div>
           </div>
+          <div className="text-sm opacity-50 font-thin leading-4">{desc}</div>
         </div>
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,42 +2,62 @@
 import ProjectCard from "./components/ProjectCard";
 import Filter from "./components/Filter";
 import Intro from "./components/Intro";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export default function Page() {
+  const [projectsdata, setProjects] = useState(null);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await fetch(
+          "https://erfansefat.github.io/ProjectData.json"
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setProjects(data);
+      } catch (error) {
+        console.error("Fetch Projects failed:", error);
+      }
+    };
+    fetchProjects();
+  }, []);
   const a = [
-    { imgsrc: "/frame1.png", key: 1 },
+    { imgsrc: "/cover_design.png", key: 1 },
     { imgsrc: "/frame2.png", key: 2 },
     { imgsrc: "/frame3.png", key: 3 },
   ];
   const bg = useRef<HTMLDivElement>(null);
-  function changecolor({ url }: { url: string }) {
+  function changecolor({ color }: { color: string }) {
     if (bg.current) {
-      bg.current.style.backgroundImage = `url(${url})`;
+      bg.current.style.backgroundColor = `${color}`;
     }
   }
   function resetcolor() {
-    if (bg.current) bg.current.style.backgroundImage = "url(/empty.png)";
+    if (bg.current) bg.current.style.backgroundColor = "#161616";
   }
 
   return (
     <div className="flex flex-col items-center ">
       <div
-        className="w-dvw h-dvh fixed bg-no-repeat bg-center rounded-[1000px] bg-cover blur-[300px] -z-20 opacity-50  brightness-50"
+        className="w-dvw h-dvh fixed rounded-[50%] blur-[300px] opacity-50"
         ref={bg}
       ></div>
-      
+
       <Filter />
       <div className="flex flex-wrap gap-5 max-w-[800px] justify-center mx-5">
-        {a.map((e) => (
+        {projectsdata?.Projects?.map((e: any) => (
           <ProjectCard
-            overfunc={() => changecolor({ url: e.imgsrc })}
+            overfunc={() => changecolor({ color: e.color })}
             leavefunc={() => resetcolor()}
             key={e.key} // Ensure each element has a unique key
-            name="Project's Title"
-            desc="This is a description"
-            year="2020"
-            type="Design"
+            name={e.title}
+            desc={e.desc}
+            year={e.year}
+            type={e.type}
+            link={e.link}
             imgsrc={e.imgsrc}
           />
         ))}
